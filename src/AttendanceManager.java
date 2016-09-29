aabeb938cb19a7a3cd3438fa6140026fd33f9e20
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2016-09-29 21:37:07 +0530

    My Second Commit.

--- src/AttendanceManager.java
+++ src/AttendanceManager.java
@@ -15,23 +15,44 @@ public class AttendanceManager {
         String subjects[]=(String [])jsObject.get("Subjects");
         File f= new File(fileName);
         if(!f.exists()){
-            fileWrite(name,age,semester,rollNumber);
+            fileWrite(name,age,semester,rollNumber,subjects);
         }
         else {
             f.delete();
-            fileWrite(name, age, semester, rollNumber);
+            fileWrite(name, age, semester, rollNumber,subjects);
         }
-        try{
-            FileWriter obj1= new FileWriter(fileName1,false);
-            BufferedWriter obj2=new BufferedWriter(obj1);
-            PrintWriter obj3=new PrintWriter(obj2);
-            JSONObject attendance= (JSONObject)jsObject.get("attendance");
-            String attendanceRecord[]= new String[subjects.length];
-            for(int i=0;i<subjects.length;i++) {
+        try {
+            FileWriter obj1 = new FileWriter(fileName1, false);
+            BufferedWriter obj2 = new BufferedWriter(obj1);
+            PrintWriter obj3 = new PrintWriter(obj2);
+            JSONObject attendance = (JSONObject) jsObject.get("attendance");
+            String attendanceRecord[] = new String[subjects.length];
+            for (int i = 0; i < subjects.length; i++) {
                 attendanceRecord[i] = (String) attendance.get(subjects[i]);
             }
-            for(int i=0;i<subjects.length;i++){
-                obj3.print(subjects[i]+": "+attendanceRecord[i]);
+            for (int i = 0; i < subjects.length; i++) {
+                obj3.print(subjects[i] + ": " + attendanceRecord[i]);
+                obj3.println();
             }
+            obj3.close();
+        }
+        catch(Exception e){System.out.println("ERROR: "+e);}
+    }
+    public void fileWrite(String name,String age,String semester, String rollNumber,String subjects[]){
+        try {
+            FileWriter obj1 = new FileWriter(fileName, false);
+            BufferedWriter obj2 = new BufferedWriter(obj1);
+            PrintWriter obj3 = new PrintWriter(obj2);
+            obj3.println("Name: " + name);
+            obj3.println("Age: " + age);
+            obj3.println("Semester: " + semester);
+            obj3.println("Roll Number: " + rollNumber);
+            obj3.println("Subjects: ");
+            for (int i = 0; i < subjects.length; i++) {
+                obj3.print(subjects[i] + ", ");
+            }
+            obj3.close();
+        }
+        catch (Exception e){System.out.println("ERROR: "+e);}
     }
 }
